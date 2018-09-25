c1a953eb51616ee1f20c3b889522e399223f112a
Heather Miller <heather.miller114@gmail.com>
2018-09-25 18:12:47 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,6 +29,10 @@ class Transfer
   end
   
   def reverse_transfer
+    if @status = "complete"
+      sender.balance += @amount
+      receiver.balance -= @amount
+      @status = "pending"
   end
 
 end
