e771b97ec382cb99b42a07c93896004cb08f3131
Heather Miller <heather.miller114@gmail.com>
2018-09-25 18:10:06 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,8 @@ class Transfer
       sender.balance -= @amount
       receiver.balance += @amount
       @status = "complete"
-    elsif valid? != true
+    else
+      @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
   end
